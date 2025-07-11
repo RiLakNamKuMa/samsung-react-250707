ed10d48756800f40cc070def6babad8faac97894
Namgu Kang <tarenamgu@gmail.com>
2025-07-11 11:10:16 +0900

    Todo 항목 추가

--- src/components/todos/TodoList.tsx
+++ src/components/todos/TodoList.tsx
@@ -1,9 +1,11 @@
 import { useEffect } from 'react'
 import { useTodoStore } from '@/stores/todo'
+import Loader from '@/components/Loader'
 
 export default function TodoList() {
   // [250711] 하나씩 가져오기
   const todos = useTodoStore(state => state.todos)
+  const isLoadingForFetch = useTodoStore(state => state.isLoadingForFetch)
   const fetchTodos = useTodoStore(state => state.fetchTodos)
 
   // [250711] 의존성 배열 추가를 위해 useEffect 사용
@@ -12,9 +14,11 @@ export default function TodoList() {
     // eslint-disable-next-line
   }, [])
 
+  // [250711] fetch 호출 시 로더 추가
   return (
     <>
       <ul>
+        {isLoadingForFetch && <Loader size={100} />}
         {todos.map(todo => {
           return <li key={todo.id}>{todo.title}</li>
         })}
--- src/stores/todo.ts
+++ src/stores/todo.ts
@@ -19,25 +19,36 @@ interface Todo {
   updatedAt: string // 할 일 수정일
 }
 
-// VITE_APIKEY = 'KDT8_bcAWVpD8'
-// VITE_USERNAME = 'KDT8_ParkYoungWoong'
+// [250711] 중복 코드 최소화를 위한 Rest API Template 만들기
+const api = axios.create({
+  baseURL: 'https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos',
+  headers: {
+    'content-type': 'application/json',
+    apikey: import.meta.env.VITE_APIKEY,
+    username: import.meta.env.VITE_USERNAME
+  }
+})
 
 export const useTodoStore = create(
-  combine({ todos: [] as Todos }, set => {
+  combine({ todos: [] as Todos, isLoadingForFetch: true }, set => {
     // [250710] Todo List 갱신을 위한 비동기 함수 추가
     return {
       fetchTodos: async () => {
-        const { data } = await axios({
-          url: 'https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos',
-          method: 'GET',
-          headers: {
-            'content-type': 'application/json',
-            apikey: import.meta.env.VITE_APIKEY,
-            username: import.meta.env.VITE_USERNAME
+        // [250711] 가져올 때 마다 true 설정
+        set({ isLoadingForFetch: true })
+        const { data } = await api({ method: 'GET' })
+        // [250711] 가져오고 나서 false 설정
+        set({ todos: data, isLoadingForFetch: false })
+      },
+      // [250711] Todo 항목 추가
+      // https://www.heropy.dev/p/QOWqjV#h3_%EC%82%AC%EC%9A%A9%EC%9E%90_%EC%A0%95%EC%9D%98_%EA%B5%AC%EC%84%B1
+      createTodo: async (title: string) => {
+        const { data } = await api({ method: 'POST', data: { title } })
+        set(state => {
+          return {
+            todos: [data, ...state.todos]
           }
         })
-        console.log(data)
-        set({ todos: data })
       }
     }
   })
